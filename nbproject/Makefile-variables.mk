c9e55277aec9ce680cfd05774358a184b12a95c9
slayer <slayer@1ddb354d-3e0c-0410-833a-d70222086f97>
2009-06-19 07:28:54 +0000

    - Update project again to link freeimage in windows
    git-svn-id: http://mancubus.net/svn/hosted/slade/trunk@238 1ddb354d-3e0c-0410-833a-d70222086f97

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,34 +7,34 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Unix_Debug configuration
-CND_PLATFORM_Unix_Debug=GNU-Linux-x86
+CND_PLATFORM_Unix_Debug=
 CND_ARTIFACT_DIR_Unix_Debug=dist
 CND_ARTIFACT_NAME_Unix_Debug=SLADE
 CND_ARTIFACT_PATH_Unix_Debug=dist/SLADE
-CND_PACKAGE_DIR_Unix_Debug=dist/Unix_Debug/GNU-Linux-x86/package
+CND_PACKAGE_DIR_Unix_Debug=dist/Unix_Debug//package
 CND_PACKAGE_NAME_Unix_Debug=trunk.tar
-CND_PACKAGE_PATH_Unix_Debug=dist/Unix_Debug/GNU-Linux-x86/package/trunk.tar
+CND_PACKAGE_PATH_Unix_Debug=dist/Unix_Debug//package/trunk.tar
 # Unix_Release configuration
-CND_PLATFORM_Unix_Release=GNU-Linux-x86
+CND_PLATFORM_Unix_Release=
 CND_ARTIFACT_DIR_Unix_Release=dist
 CND_ARTIFACT_NAME_Unix_Release=SLADE
 CND_ARTIFACT_PATH_Unix_Release=dist/SLADE
-CND_PACKAGE_DIR_Unix_Release=dist/Unix_Release/GNU-Linux-x86/package
+CND_PACKAGE_DIR_Unix_Release=dist/Unix_Release//package
 CND_PACKAGE_NAME_Unix_Release=trunk.tar
-CND_PACKAGE_PATH_Unix_Release=dist/Unix_Release/GNU-Linux-x86/package/trunk.tar
+CND_PACKAGE_PATH_Unix_Release=dist/Unix_Release//package/trunk.tar
 # Win32_Release configuration
-CND_PLATFORM_Win32_Release=
+CND_PLATFORM_Win32_Release=MinGW-Windows
 CND_ARTIFACT_DIR_Win32_Release=dist
 CND_ARTIFACT_NAME_Win32_Release=SLADE
 CND_ARTIFACT_PATH_Win32_Release=dist/SLADE
-CND_PACKAGE_DIR_Win32_Release=dist/Win32_Release//package
+CND_PACKAGE_DIR_Win32_Release=dist/Win32_Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Win32_Release=trunk.tar
-CND_PACKAGE_PATH_Win32_Release=dist/Win32_Release//package/trunk.tar
+CND_PACKAGE_PATH_Win32_Release=dist/Win32_Release/MinGW-Windows/package/trunk.tar
 # Win32_Debug configuration
-CND_PLATFORM_Win32_Debug=
+CND_PLATFORM_Win32_Debug=MinGW-Windows
 CND_ARTIFACT_DIR_Win32_Debug=dist
 CND_ARTIFACT_NAME_Win32_Debug=SLADE
 CND_ARTIFACT_PATH_Win32_Debug=dist/SLADE
-CND_PACKAGE_DIR_Win32_Debug=dist/Win32_Debug//package
+CND_PACKAGE_DIR_Win32_Debug=dist/Win32_Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Win32_Debug=trunk.tar
-CND_PACKAGE_PATH_Win32_Debug=dist/Win32_Debug//package/trunk.tar
+CND_PACKAGE_PATH_Win32_Debug=dist/Win32_Debug/MinGW-Windows/package/trunk.tar
