d0ce088e7041d35069abf537194ef7b6caa3b3d5
slayer <slayer@1ddb354d-3e0c-0410-833a-d70222086f97>
2010-03-09 14:51:56 +0000

    - Set colour to white before drawing GfxCanvas background
    git-svn-id: http://mancubus.net/svn/hosted/slade/trunk@369 1ddb354d-3e0c-0410-833a-d70222086f97

--- src/GfxCanvas.cpp
+++ src/GfxCanvas.cpp
@@ -181,6 +181,7 @@ void GfxCanvas::drawChequeredBackground() {
 
 	// Draw background
 	frect_t rect(0, 0, GetSize().x, GetSize().y);
+	COL_WHITE.set_gl();
 	glBegin(GL_QUADS);
 	glTexCoord2d(rect.x1()*0.0625, rect.y1()*0.0625);	glVertex2d(rect.x1(), rect.y1());
 	glTexCoord2d(rect.x1()*0.0625, rect.y2()*0.0625);	glVertex2d(rect.x1(), rect.y2());
@@ -190,35 +191,6 @@ void GfxCanvas::drawChequeredBackground() {
 
 	// Disable textures
 	glDisable(GL_TEXTURE_2D);
-
-	/*
-	// Determine the number of rows and columns
-	double s_size = 8.0;
-	int cols = int((double)GetSize().x / s_size) + 1;
-	int rows = int((double)GetSize().y / s_size) + 1;
-
-	// Scale to square size
-	glScaled(s_size, s_size, 1);
-
-	// Draw a grid of squares
-	for (int x = 0; x < cols; x++) {
-		for (int y = 0; y < rows; y++) {
-			// Set square colour
-			rgba_t col(64, 64, 80, 255);
-			if (x%2 != y%2)
-				col.set(80, 80, 96, 255);
-			col.set_gl();
-
-			// Draw the square
-			glBegin(GL_QUADS);
-			glVertex2d(x, y);
-			glVertex2d(x, y+1);
-			glVertex2d(x+1, y+1);
-			glVertex2d(x+1, y);
-			glEnd();
-		}
-	}
-	*/
 
 	// Restore previous matrix
 	glPopMatrix();
