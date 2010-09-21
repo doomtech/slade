7d0b75a18aa902f6a8a2add120728395d4d6481b
slayer <slayer@1ddb354d-3e0c-0410-833a-d70222086f97>
2010-09-21 06:07:42 +0000

    - Added basic playback controls for the audio panel in wxgtk (gstreamer has no builtin controls) - Updated codelite project - Updated README-unix with new wxWidgets build instructions for wxMediaCtrl support
    git-svn-id: http://mancubus.net/svn/hosted/slade/trunk@667 1ddb354d-3e0c-0410-833a-d70222086f97

--- src/AudioEntryPanel.cpp
+++ src/AudioEntryPanel.cpp
@@ -2,13 +2,28 @@
 #include "Main.h"
 #include "WxStuff.h"
 #include "AudioEntryPanel.h"
+#include "Icons.h"
 #include <wx/filename.h>
 
 AudioEntryPanel::AudioEntryPanel(wxWindow* parent) : EntryPanel(parent, "audio") {
 	// Add media player control
-	media_player = new wxMediaCtrl(this, -1, wxEmptyString);
+	media_player = new wxMediaCtrl(this, -1, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxBORDER_SIMPLE);
 	media_player->ShowPlayerControls();
-	sizer_main->Add(media_player, 1, wxEXPAND|wxALL, 4);
+	sizer_main->Add(media_player, 1, wxEXPAND);
+
+	// Add play controls if needed
+#ifdef __WXGTK__
+	btn_play = new wxBitmapButton(this, -1, getIcon("i_play"));
+	sizer_bottom->Add(btn_play, 0, wxEXPAND|wxRIGHT, 4);
+	btn_pause = new wxBitmapButton(this, -1, getIcon("i_pause"));
+	sizer_bottom->Add(btn_pause, 0, wxEXPAND|wxRIGHT, 4);
+	btn_stop = new wxBitmapButton(this, -1, getIcon("i_stop"));
+	sizer_bottom->Add(btn_stop, 0, wxEXPAND|wxRIGHT, 4);
+
+	btn_play->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &AudioEntryPanel::onBtnPlay, this);
+	btn_pause->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &AudioEntryPanel::onBtnPause, this);
+	btn_stop->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &AudioEntryPanel::onBtnStop, this);
+#endif
 
 	Layout();
 }
@@ -37,4 +52,17 @@ bool AudioEntryPanel::loadEntry(ArchiveEntry* entry) {
 
 bool AudioEntryPanel::saveEntry() {
 	return true;
+}
+
+
+void AudioEntryPanel::onBtnPlay(wxCommandEvent& e) {
+	media_player->Play();
+}
+
+void AudioEntryPanel::onBtnPause(wxCommandEvent& e) {
+	media_player->Pause();
+}
+
+void AudioEntryPanel::onBtnStop(wxCommandEvent& e) {
+	media_player->Stop();
 }
--- src/AudioEntryPanel.h
+++ src/AudioEntryPanel.h
@@ -10,12 +10,21 @@ private:
 	wxMediaCtrl*	media_player;
 	string			prevfile;
 
+	wxBitmapButton*	btn_play;
+	wxBitmapButton*	btn_pause;
+	wxBitmapButton*	btn_stop;
+
 public:
 	AudioEntryPanel(wxWindow* parent);
 	~AudioEntryPanel();
 
 	bool	loadEntry(ArchiveEntry* entry);
 	bool	saveEntry();
+
+	// Events
+	void	onBtnPlay(wxCommandEvent& e);
+	void	onBtnPause(wxCommandEvent& e);
+	void	onBtnStop(wxCommandEvent& e);
 };
 
 #endif//__AUDIO_ENTRY_PANEL_H__
