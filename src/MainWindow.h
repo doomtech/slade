e973676525d54b8af344adcac893ca620dd03987
slayer <slayer@1ddb354d-3e0c-0410-833a-d70222086f97>
2010-10-05 03:53:13 +0000

    - Updated main window layout version number (palette chooser won't show up in the toolbar with a cfg from a previous version otherwise)
    git-svn-id: http://mancubus.net/svn/hosted/slade/trunk@710 1ddb354d-3e0c-0410-833a-d70222086f97

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -12,7 +12,7 @@
 // then a previously saved perspective is loaded from slade3.cfg, the new item won't show
 // up at all. So when saving the perspective, add this number to the start of the string,
 // and check against it when loading. In other words, wxAUI is silly.
-#define MW_LAYOUT_VERS 004
+#define MW_LAYOUT_VERS 005
 
 class MainWindow : public wxFrame {
 private:
