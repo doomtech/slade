9f8fe218ad29743fde763220de54937e82ba4747
slayer <slayer@1ddb354d-3e0c-0410-833a-d70222086f97>
2012-07-04 12:32:40 +0000

    - Updated CodeLite project - Fix for gcc compilation
    git-svn-id: http://mancubus.net/svn/hosted/slade/trunk@1332 1ddb354d-3e0c-0410-833a-d70222086f97

--- src/TextEditor.cpp
+++ src/TextEditor.cpp
@@ -235,7 +235,8 @@ void TextEditor::setup() {
 	StyleSet::applyCurrent(this);
 	CallTipUseStyle(10);
 	StyleSetChangeable(wxSTC_STYLE_CALLTIP, true);
-	StyleSetFont(wxSTC_STYLE_CALLTIP, wxFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+	wxFont font_ct(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+	StyleSetFont(wxSTC_STYLE_CALLTIP, font_ct);
 
 	// Set lexer
 	if (txed_syntax_hilight)
